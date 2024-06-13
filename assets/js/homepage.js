be0277e0326016fd9cb594ea78b19efda4c9288d
Aileen Nguyen <aileenn@gmail.com>
2024-06-12 21:26:40 -0700

    added dates

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,117 +1,42 @@
-//date variables
+//showing dates of forecast
 $(document).ready(function () {
   var currentDate = document.querySelector("#currentDay");
   var showDate = dayjs().format("MMMM D");
 
   currentDate.textContent = showDate;
-
-  const day1 = currentDate.add(7, "day");
 });
 
-var userFormEl = document.querySelector("#user-form");
-var languageButtonsEl = document.querySelector("#language-buttons");
-var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+$(document).ready(function () {
+  var currentDate = document.querySelector("#day1");
+  var day1 = dayjs().add(1, "day").format("MMMM D");
 
-var formSubmitHandler = function (event) {
-  event.preventDefault();
+  currentDate.textContent = day1;
+});
 
-  var username = nameInputEl.value.trim();
+$(document).ready(function () {
+  var currentDate = document.querySelector("#day2");
+  var day2 = dayjs().add(2, "day").format("MMMM D");
 
-  if (username) {
-    getUserRepos(username);
+  currentDate.textContent = day2;
+});
 
-    repoContainerEl.textContent = "";
-    nameInputEl.value = "";
-  } else {
-    alert("Please enter a GitHub username");
-  }
-};
+$(document).ready(function () {
+  var currentDate = document.querySelector("#day3");
+  var day3 = dayjs().add(3, "day").format("MMMM D");
 
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute("data-language");
+  currentDate.textContent = day3;
+});
 
-  if (language) {
-    getFeaturedRepos(language);
+$(document).ready(function () {
+  var currentDate = document.querySelector("#day4");
+  var day4 = dayjs().add(4, "day").format("MMMM D");
 
-    repoContainerEl.textContent = "";
-  }
-};
+  currentDate.textContent = day4;
+});
 
-var getUserRepos = function (user) {
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+$(document).ready(function () {
+  var currentDate = document.querySelector("#day5");
+  var day5 = dayjs().add(5, "day").format("MMMM D");
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert("Unable to connect to GitHub");
-    });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl =
-    "https://api.github.com/search/repositories?q=" +
-    language +
-    "+is:featured&sort=help-wanted-issues";
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = "No repositories found.";
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
-
-    var repoEl = document.createElement("a");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
-
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" +
-        repos[i].open_issues_count +
-        " issue(s)";
-    } else {
-      statusEl.innerHTML =
-        "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-
-userFormEl.addEventListener("submit", formSubmitHandler);
-languageButtonsEl.addEventListener("click", buttonClickHandler);
+  currentDate.textContent = day5;
+});
